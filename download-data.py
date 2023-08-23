81660b54fb480dc6f9b9b6c537e69199e1b6395c
petrzpav <pavel@petrzela.eu>
2023-08-23 08:04:24 +0200

    Working download and json2csv

---
+++ download-data.py
@@ -0,0 +1,92 @@
+#!/bin/env python3
+
+import os
+import argparse
+import urllib.request
+import base64
+import json
+from datetime import datetime
+
+REQUEST_URL_TEMPLATE = 'https://app.fakturoid.cz/api/v2/accounts/{}/{}'
+
+def get_auth():
+    """
+    Get user slug, username, API key and store it .auth file. If there is already .auth file, use it.
+    """
+    auth_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".auth")
+    # Check if .auth file already exists
+    if os.path.exists(auth_path):
+        with open(auth_path, 'r') as file:
+            lines = file.readlines()
+            slug = lines[0].strip()
+            username = lines[1].strip()
+            api_key = lines[2].strip()
+            return [slug, username, api_key]
+    # If .auth file doesn't exist, prompt the user for details
+    slug = input("Enter slug: ")
+    username = input("Enter username: ")
+    api_key = input("Enter API key: ")
+    # Save the details in .auth file
+    with open(auth_path, 'w') as file:
+        file.write(slug + '\n')
+        file.write(username + '\n')
+        file.write(api_key + '\n')
+    return [slug, username, api_key]
+
+def download_all(type, since):
+    """
+    Download paginated data from API, ask for since date.
+    """
+    auth_data = get_auth()
+    all_data = []
+    api_url = REQUEST_URL_TEMPLATE.format(auth_data[0], type)
+    if since:
+        api_url = api_url + '?since={}'.format(datetime.strptime(since, '%Y-%m-%d').isoformat())
+    while api_url:
+        # set auth header
+        request = urllib.request.Request(api_url)
+        base64string = base64.b64encode(bytes('%s:%s' % (auth_data[1], auth_data[2]), 'ascii'))
+        request.add_header("Authorization", "Basic %s" % base64string.decode('utf-8'))
+        result = urllib.request.urlopen(request)
+        data = json.loads(result.read().decode("utf-8"))
+        all_data += data
+        api_url = None
+        # get next api_url for paginated data
+        link_header = result.getheader('Link')
+        # print(result.info())
+        if link_header:
+            # Splitting the Link header value to get individual links
+            links = link_header.split(',')
+            next_link = None
+            for link in links:
+                # Extracting URL and rel type
+                parts = link.split(';')
+                url = parts[0].strip()[1:-1]  # Remove < and >
+                rel = parts[1].strip()
+                if 'next' == rel:
+                    next_link = url
+                    break
+            api_url = next_link
+    print(json.dumps(all_data))
+
+parser = argparse.ArgumentParser(
+    prog='FakturoidDownloader',
+    description='Download requested data from Fakturoid API and prints them to the output.',
+)
+parser.add_argument(
+    '-t', '--type',
+    required=True,
+    choices=['invoices', 'expenses'],
+    action='store',
+    help='Choose a Fakturoid data type to download.'
+)
+parser.add_argument(
+    '-s', '--since',
+    action='store',
+    help='Download since, e.g. 2021-01-01 (empty for no date).'
+)
+args = parser.parse_args()
+
+match args.type:
+    case 'invoices' | 'expenses':
+        download_all('{}.json'.format(args.type), args.since)
---
+++ json2csv.py
@@ -0,0 +1,59 @@
+#!/bin/env python3
+
+import csv
+import json
+import sys
+from io import StringIO
+
+def flatten(json_object, parent_key='', sep='.'):
+    """
+    Recursively flatten a JSON object.
+    """
+    items = {}
+    # If the json_object is a dictionary
+    if isinstance(json_object, dict):
+        for k, v in json_object.items():
+            new_key = parent_key + sep + k if parent_key else k
+            # If the value is a dictionary, recursively flatten
+            if isinstance(v, dict):
+                items.update(flatten(v, new_key, sep=sep))
+            # If the value is a list
+            elif isinstance(v, list):
+                for idx, elem in enumerate(v):
+                    # Construct a new key based on the index
+                    list_key = new_key + sep + str(idx)
+                    if isinstance(elem, (dict, list)):
+                        items.update(flatten(elem, list_key, sep=sep))
+                    else:
+                        items[list_key] = elem
+            else:
+                items[new_key] = v
+    return items
+
+def json_to_csv_output(json_data):
+    """
+    Convert a list of JSON objects to CSV and output to console.
+    """
+    # Convert each JSON object into a flattened dictionary
+    flattened_data = []
+    for item in json_data:
+        if isinstance(item, dict):
+            flattened_data.append(flatten(item))
+    # Check if there's any data to write
+    if not flattened_data:
+        print("No valid data found to convert to CSV.")
+        return
+    # Use StringIO to simulate a file object
+    output = StringIO()
+    writer = csv.DictWriter(output, fieldnames=flattened_data[0].keys())
+    writer.writeheader()
+    for row in flattened_data:
+        writer.writerow(row)
+    # Move the cursor to the beginning of the "file" and print its contents
+    output.seek(0)
+    print(output.read())
+
+# Read JSON from stdin
+json_str = sys.stdin.read()
+# Convert and output to console
+json_to_csv_output(json.loads(json_str))
